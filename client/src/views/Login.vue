7ea4c62126c86c7533e62572035b1188f43545f9
GADLO <geekarea@outlook.com>
2023-03-30 16:34:18 +0800

    新增vue3客户端

---
+++ client/src/views/Login.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+    <h1>登录</h1>
+    <p>
+        <input type="text" placeholder="用户名" v-model="username">
+    </p>
+    <p>
+        <input type="text" placeholder="密码" v-model="password">
+    </p>
+    <p>
+        <router-link to="/register" >
+        注册
+        </router-link>
+
+        <button @click="submitLogin" >
+        登录
+        </button>
+    </p>
+</div>
+</template>
+
+<script setup lang='ts'>
+
+</script>
+
+<style lang='scss'>
+ 
+</style>
